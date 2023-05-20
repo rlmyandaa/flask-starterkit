bada8b25a98d9297388a2cd723d40191ef2ff83c
Hersyanda Putra Adi <49233604+rlmyandaa@users.noreply.github.com>
2023-05-20 10:32:55 +0700

    missing utils

--- app/ext/sqlalchemy/database.py
+++ app/ext/sqlalchemy/database.py
@@ -2,13 +2,21 @@
 # https://flask.palletsprojects.com/en/2.1.x/patterns/sqlalchemy/
 # https://docs.sqlalchemy.org/en/14/orm/session_basics.html#session-getting
 
-from flask import Flask
+from flask import Flask, current_app
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 # create empty session for future usage
 db_session: scoped_session = scoped_session(sessionmaker())
+
+
+def get_engine() -> Engine:
+    engine: Engine = create_engine(
+        current_app.config.get("SQLALCHEMY_DATABASE_URI"),
+        echo=current_app.config.get("SQLALCHEMY_ENGINE_ECHO")
+    )
+    return engine
 
 
 def init_database(app: Flask) -> None:
